7bf44075daf9ad558ad7a2b83849e2cb562db01a
Yasin Ünal <yasin.unal@bil.omu.edu.tr>
2023-08-03 15:06:13 +0300

    spotify api kaldırıldı

---
+++ keys.js
@@ -0,0 +1,2 @@
+const clientId = 'e79bdf75e15f40bf988fb0115dad338d';
+const clientSecret = 'eb1701e836bb410c86df374163c828d6';
--- server/main.js
+++ server/main.js
@@ -43,8 +43,7 @@ Meteor.methods({'list_user' : function(){
 
 
 
-const clientId = 'e79bdf75e15f40bf988fb0115dad338d';
-const clientSecret = 'eb1701e836bb410c86df374163c828d6';
+
 
 
 
